cb35a705248a514816fd6d6914ea23e22b3c45dc
vinhsuhi <vinhbachkhoait@gmail.com>
2020-03-04 22:56:47 +0700

    input service as prefix

--- services/LoadData.py
+++ services/LoadData.py
@@ -1,6 +1,8 @@
 from utils import say_goodbye, say_hello, create_dics, load_files, save_to_audio, save_and_say, change_the_mean
 
-def getData(path_to_eng, path_to_vie):
+def getData(prefix):
+    path_to_eng = '{}e.txt'.format(prefix)
+    path_to_vie = '{}v.txt'.format(prefix)
     engs, viets = load_files(path_to_eng, path_to_vie)
     save_to_audio(engs, 'en')
 
